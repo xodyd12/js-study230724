d1fb4378f55a4bbcda87cd2f9fb38cf8acd541e1
Hantaeyong <hhhhgong3@naver.com>
2023-07-24 16:03:45 +0900

    basic 자바 연습

---
+++ 1.basic/3.상술연산자.js
@@ -0,0 +1,31 @@
+let num = 3;
+console.log(num **4);
+
+//증감 연산자
+var x =5;
+ 
+x++;
+console.log(`x: &{x}`);
+
+let y =33;
+console.log(`y: ${--y}`);
+
+let a = 7;
+let b = a++;
+console.log(`a:${a},b:${b}`);
+let c= 7;
+let d = ++c;
+
+console.log(`a:${a},b:${b}`);
+
+let z = 9;
+z = z + 1;
+z -= 8;
+z *= 2;
+z /= 8;
+z **= 2;
+console.log(`z: ${z}`);
+
+let m = 8;
+m ++;
+console.log(`m: ${m}`);
