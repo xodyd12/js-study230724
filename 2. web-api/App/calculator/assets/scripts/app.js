7abf4c1dbc1d56e84d76a3bd4ddc62f90422f95b
Hantaeyong <hhhhgong3@naver.com>
2023-08-02 13:28:01 +0900

    web api -실전예제 1 : 로고 렌더링 기능 추가

--- 2. web-api/App/calculator/assets/scripts/app.js
+++ 2. web-api/App/calculator/assets/scripts/app.js
@@ -9,15 +9,17 @@ let currentResult = 0;
 
 // 로그 이력을 만드는 함수
 const writeToLog = (operation, prevResult, number, result) => {
-  // 하나의 로그 객체 (연산, 이전결과, 연산숫자, 연산결과)
-  const logObject = {
-    operation,
-    prevResult,
-    number,
-    result 
-  };
-  logEntries.push(logObject);
-  console.log(logEntries);
+    // 하나의 로그 객체 (연산, 이전결과, 연산숫자, 연산결과)
+    const logObject = {
+        operation,
+        prevResult,
+        number,
+        result 
+    };
+    logEntries.push(logObject);
+    const $Logli = $resultlogIn.appendChild(document.createElement('li'));
+    $Logli.textContent = (`${logObject.operation}, ${logObject.prevResult}, ${logObject.number}, ${logObject.result}`);
+
 };
 
 // 사용자의 입력값을 읽어오는 함수
@@ -29,35 +31,37 @@ const createLogAndRenderOutput = (operateMark, originResult, calcNumber) => {
   const calcDescriptionLog = `${originResult} ${operateMark} ${calcNumber}`;
   // 화면에 렌더링
   outputResult(currentResult, calcDescriptionLog);
+  
 };
 
 // 계산 기능 헬퍼 함수
 const calculate = (type) => {
-  // 계산 전 값을 백업
-  const originResult = currentResult;
-  // 입력창에 입력한 숫자를 읽기
-  const enteredNumber = getUserNumerInput();
-
-  let mark;
-  if (type === 'ADD') {
-    mark = '+';
-    // 실제 계산 결과 반영
-    currentResult += enteredNumber;
-  } else if (type === 'SUB') {
-    mark = '-';
-    currentResult -= enteredNumber;
-  } else if (type === 'MULTI') {
-    mark = 'x';
-    currentResult *= enteredNumber;
-  } else {
-    mark = '/';
-    currentResult /= enteredNumber;
-  }
-
-  console.log(`${mark} button click!`);
-  createLogAndRenderOutput(mark, originResult, enteredNumber);
-  // 로그이력 쌓기
-  writeToLog(type, originResult, enteredNumber, currentResult);
+    // 계산 전 값을 백업
+    const originResult = currentResult;
+    // 입력창에 입력한 숫자를 읽기
+    const enteredNumber = getUserNumerInput();
+    
+    let mark;
+    if (type === 'ADD') {
+        mark = '+';
+        // 실제 계산 결과 반영
+        currentResult += enteredNumber;
+    } else if (type === 'SUB') {
+        mark = '-';
+        currentResult -= enteredNumber;
+    } else if (type === 'MULTI') {
+        mark = 'x';
+        currentResult *= enteredNumber;
+    } else {
+        mark = '/';
+        currentResult /= enteredNumber;
+    }
+    
+    console.log(`${mark} button click!`);
+    createLogAndRenderOutput(mark, originResult, enteredNumber);
+    // 로그이력 쌓기
+    writeToLog(type, originResult, enteredNumber, currentResult);
+    
 };
 
 
--- 2. web-api/App/calculator/assets/scripts/vendor.js
+++ 2. web-api/App/calculator/assets/scripts/vendor.js
@@ -10,8 +10,10 @@ const $divideBtn = document.getElementById('btn-divide');
 const $currentResultOutput = document.getElementById('current-result');
 const $currentCalculationOutput = document.getElementById('current-calculation');
 
+const $resultlogIn = document.getElementById('logIn')
 
 function outputResult(result, text) {
   $currentResultOutput.textContent = result;
   $currentCalculationOutput.textContent = text;
 }
+
