f20aad954c419c84f4c97bae87108cf46db6faed
Hantaeyong <hhhhgong3@naver.com>
2023-08-07 09:12:12 +0900

    web api - 실전예제 3 쇼핑 완성본

--- 2. web-api/App/Shopping/assets/scripts/app.js
+++ 2. web-api/App/Shopping/assets/scripts/app.js
@@ -24,63 +24,100 @@ class Product {
   );
   // console.log(p2);
   
+  // 속성 객체를 만드는 클래스
+  class Attribute {
+    constructor(name, value) {
+      this.name = name;
+      this.value = value;
+    }
+  }
+  
+  // DOM을 생성하는 공통 클래스
+  class Component {
+    // tagId : 부모태그 역할을 할 태그의 id속성값
+    constructor(tagId) {
+      this.tagId = tagId;
+    }
+    // 태그를 생성하는 함수
+    // classnames = 'box blue circle'
+    createElement(
+      tagName = "",
+      classnames = "",
+      childHtml = "",
+      attributes = []
+    ) {
+      const $newTag = document.createElement(tagName);
+      if (classnames) $newTag.className = classnames;
+      if (attributes && attributes.length > 0) {
+        attributes.forEach((attr) => {
+          $newTag.setAttribute(attr.name, attr.value);
+        });
+      }
+      if (childHtml) $newTag.innerHTML = childHtml;
+      document.getElementById(this.tagId).appendChild($newTag);
+      return $newTag;
+    }
+  }
   
   // 화면 가장 상단부에 들어갈 장바구니 총액 정보 태그 생성 클래스
-  class ShoppingCart {
-    constructor() {
+  class ShoppingCart extends Component {
+    constructor(tagId) {
+      super(tagId);
       // 장바구니에 담은 Product들을 저장
       this.cartItems = [];
     }
   
     render() {
-      const $cart = document.createElement('section');
-      $cart.classList.add('cart');
-      $cart.innerHTML = `
+      const childHtml = `
         <h2>총액 0원</h2>
         <button>주문하기</button>
       `;
-      return $cart;
+      this.createElement("section", "cart", childHtml, [
+        new Attribute("id", "cart-id"),
+        new Attribute("title", "add to cart"),
+      ]);
     }
   }
   
-  
   // 한개의 LI태그를 생성하는 컴포넌트 클래스 설계
-  class ProductItem {
-    constructor(product) {
+  class ProductItem extends Component {
+    constructor(product, tagId) {
+      super(tagId);
       this.product = product;
     }
   
     // 담기버튼 클릭이벤트 핸들러
     addToCartHandler() {
-      console.log('장바구니에 상품을 추가함!');
+      console.log("장바구니에 상품을 추가함!");
       // 이 핸들러에서 누른 그 상품의 정보를 알아야 한다.
       console.log(this.product);
     }
   
     render() {
-      const $prod = document.createElement("li");
-      $prod.classList.add("product-item");
-      $prod.innerHTML = `
-          <div>
-            <img src="${this.product.imageUrl}" alt="${this.product.title}">
-            <div class="product-item__content">
-              <h2>${this.product.title}</h2>
-              <h3>${this.product.price}원</h3>
-              <p>${this.product.description}</p>
-              <button>담기</button>
-            </div>
-          </div>
-        `;
-      const $addCartBtn = $prod.querySelector('button');
+      const childHtml = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>${this.product.price}원</h3>
+          <p>${this.product.description}</p>
+          <button>담기</button>
+        </div>
+      </div>
+      `;
+      const $prod = this.createElement("li", "product-item", childHtml);
+      
+      const $addCartBtn = $prod.querySelector("button");
       // $addCartBtn.addEventListener('click', this.addToCartHandler.bind(this));
-      $addCartBtn.addEventListener('click', () => this.addToCartHandler());
+      $addCartBtn.addEventListener("click", () => this.addToCartHandler());
       return $prod;
     }
   }
   
   // 한 개의 UL을 생성하는 클래스
-  class ProductList {
-    constructor() {
+  class ProductList extends Component {
+    constructor(tagId) {
+      super(tagId);
       // 상품들을 모아 놓은 배열
       this.products = [
         p1,
@@ -107,15 +144,14 @@ class Product {
     } // end constructor
   
     render() {
-      // console.log('render!!', this);
-      const $prodList = document.createElement("ul");
-      $prodList.classList.add("product-list");
+      this.createElement("ul", "product-list", "", [
+        new Attribute("id", "prod-list"),
+      ]);
+  
       this.products.forEach((prod) => {
-        const productItem = new ProductItem(prod);
-        // console.log(productItem);
-        $prodList.appendChild(productItem.render());
+        const productItem = new ProductItem(prod, "prod-list");
+        productItem.render();
       });
-      return $prodList;
     }
   }
   
@@ -126,13 +162,11 @@ class Product {
     }
   
     render() {
-      const $app = document.getElementById('app');
-      $app.appendChild(new ShoppingCart().render());
-      $app.appendChild(new ProductList().render());
+      new ShoppingCart("app").render();
+      new ProductList("app").render();
     }
   }
   
   // 렌더링 명령
   new Shop();
-  
   
